eb2dac8129f07a28e445254235151a2a24b7732d
Ibrahim Abdi <ibrahimabdi2367@gmail.com>
2023-02-12 10:00:49 -0500

    homework

---
+++ app.js
@@ -0,0 +1,49 @@
+// const addTogether = (arg1, arg2) =>
+//     typeof arg1 !== 'number'
+//     ? undefined
+//     : arg2 === undefined
+//     ? num => typeof num !== 'number'
+//         ? undefined
+//         : arg1 + num
+//     : typeof arg2 !== 'number'
+//     ? undefined
+//     : arg1 + arg2;
+
+// var Person = function(firstAndLast) {
+//     let firstVar = firstAndLast.split(" ")[0];
+//     let lastVar = firstAndLast.split(" ")[1];
+
+//     this.getFullName = function() {
+//         return `${firstVar} ${lastVar}` ;
+//     };
+
+//     this.getFirstName = function() {
+//         return firstVar;
+//     };
+
+//     this.getLastName = function() {
+//         return lastVar;
+//     };
+
+//     this.setFirstName = function(first) {
+//         firstVar = first;
+//     };
+
+//     this.setlastName = function(last) {
+//         lastVar = last;
+//     };
+
+//     this.setFullName = function (firstAndLast) {
+//         firstVar = firstAndLast.split(" ")[0];
+//         lastVar = firstAndLast.split(" ")[1];
+//     }
+
+//     var Harry = new Person("Harry Potter");
+// }
+
+// function orbitalPeriod(arr) {
+//     var GM = 398600.4418;
+//     var earthRadius = 6367.4441;
+//     return arr;
+// }
+// orditalPeriod([{name : "sputnik", avgAlt : 35873.5553}]);
